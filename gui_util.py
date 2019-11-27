c9d1c4b80b59e36ef6e09b5ba8bd25891d0f3af6
eran505 <eran505@gmail.com>
2019-11-27 10:02:04 +0200

    Add files via upload

--- gui_util.py
+++ gui_util.py
@@ -9,21 +9,26 @@ from util_system import get_spped_pos
 
 class game_gui(object):
 
-    def __init__(self,x,y,window_size):
+    def __init__(self,x,y,window_size,wait_key=False,sleep_t=1000):
         self.surface=None
         self.x_axis=x
         self.y_axis=y
+        self.wait_ky=wait_key
         self.score_size=100
         self.window_size=window_size
         self.player_dico={}
         self.cube_end_list=[]
+        self.sleep_time=sleep_t
         self.ctr_coll=0
         self.ctr_at_goal=0
         self.ctr_wall=0
+        self.change_color=False
         self.constructor()
         self.GAME_FONT = None
 
     def constructor(self):
+        if self.wait_ky:
+            self.sleep_time=1
         pygame.init()
         score_size=self.score_size
         self.surface = pygame.display.set_mode((self.window_size + score_size, self.window_size + score_size))
@@ -52,29 +57,28 @@ class game_gui(object):
 
     def draw_players(self,surface):
         for ky in self.player_dico:
-            self.player_dico[ky].draw(surface,self.window_size // self.x_axis,self.window_size // self.y_axis)
+            self.player_dico[ky].draw(surface,self.window_size // self.x_axis,self.window_size // self.y_axis,self.is_end)
 
-    def event_key(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+    def wait_for_key(self):
+        while self.wait_ky:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit(0)
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_ESCAPE]:
+                    exit(0)
+                if (keys[pygame.K_RIGHT]):
+                    return
 
-            keys = pygame.key.get_pressed()
+                if (keys[pygame.K_LEFT]):
+                    return
 
-            if (keys[pygame.K_RIGHT]):
-                return (0,1)
+                if (keys[pygame.K_UP]):
+                    return
 
-            if (keys[pygame.K_LEFT]):
-                return (0, -1)
+                if keys[pygame.K_DOWN]:
+                    return
 
-            if (keys[pygame.K_UP]):
-                return (-1, 0)
-
-            if (keys[pygame.K_DOWN]):
-                return (1, 0)
-
-            if (keys[pygame.K_ESCAPE]):
-                pygame.quit()
 
     def draw_game(self):
         self.score()
@@ -99,7 +103,7 @@ class game_gui(object):
 
     def redraw_Window(self):
         self.surface.fill((0, 0, 0))
-        pygame.time.delay(1000)
+        pygame.time.delay(self.sleep_time)
         self.draw_game()
         pygame.display.update()
 
@@ -122,7 +126,7 @@ class game_gui(object):
 
 
     def update(self,state,info):
-
+        self.is_end = False
         if info == 'Wall':
             self.ctr_wall+=1
             return
@@ -131,16 +135,20 @@ class game_gui(object):
         if info == 'goal':
             self.ctr_at_goal+=1
 
+        if info is not None:
+            self.is_end = True
         for id_name in self.player_dico:
             pos_i,speed_i= get_spped_pos(state,id_name)
             self.player_dico[id_name].pos_change(pos_i)
         self.redraw_Window()
 
+        self.wait_for_key()
 
 class cube(object):
 
     def __init__(self, start, color=(255,192,203)):
 
+        self.dead_color=(255,255,0)
         self.pos = start
         self.dirnx = 0
         self.dirny = 0
@@ -155,21 +163,18 @@ class cube(object):
     def pos_change(self,pos):
         self.pos=pos
 
-    def draw(self, surface, dis_x,dis_y):
+    def draw(self, surface, dis_x,dis_y,is_dead=False):
         mergin=0
         j = self.pos[0]
         i = self.pos[1]
 
         # print(self.pos)
         # print("{}:{} {}:{}".format(i * dis_y  , j * dis_x , dis_x , dis_y ))
+        paint_color = self.color
+        if is_dead:
+            paint_color=self.dead_color
 
-
-        pygame.draw.rect(surface, self.color, (i * dis_x + mergin , j * dis_y + mergin , dis_x -mergin , dis_y - mergin ))
-
-
-
-
-
+        pygame.draw.rect(surface, paint_color, (i * dis_x + mergin , j * dis_y + mergin , dis_x -mergin , dis_y - mergin ))
 
 
 
